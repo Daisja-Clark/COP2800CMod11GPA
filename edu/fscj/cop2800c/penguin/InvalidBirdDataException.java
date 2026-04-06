03a3e13ae3ec202c770e2e14f58fb57ef1cd3fdc
Daisja clark <s3058693@students.fscj.edu>
2026-04-05 21:45:32 -0400

    complete assignment

---
+++ edu/fscj/cop2800c/penguin/InvalidBirdDataException.java
@@ -0,0 +1,12 @@
+//Invalid Bird Data Exception
+// Name: Daisja Clark
+// Date: April 5,2026
+//
+
+package edu.fscj.cop2800c.penguin;
+
+public class InvalidBirdDataException extends Exception {
+   public InvalidBirdDataException(String message) {
+      super(message);
+   }
+}
